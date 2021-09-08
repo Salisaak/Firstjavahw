af52e31e5f08d485e9114d0761a27011e75408f0
Saahir <Salisaak@gmail.com>
2021-09-08 03:24:21 -0400

    Add files via upload

---
+++ FirstJavaHomework/src/main/java/FirstJavaHomework.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class FirstJavaHomework {
+    public static void main(String[] args) {
+        System.out.println("Le world is clapped"); //Oooh magic lines make words not code
+
+        String junTitle = "Yjoltetepique";
+        System.out.println(junTitle);
+
+        int guap;           //-2,147,483,648 to 2,147,483,647
+        guap = -445;
+        System.out.println(guap);
+
+        byte snack = 27;        //-128 to 127
+        System.out.println(snack);
+
+        short stack = -17380;     //-32768 to 32767
+        System.out.println(stack);
+
+        int dedfood = 4446907;         //-2147483648 to 2147483647
+        System.out.println(dedfood);
+
+        long nights = 302321743;      //-9223372036854775808 to 9223372036854775807
+        System.out.println(nights);
+
+
+        float yaBoat = 4.20f;      //fractional numbers from 3.4e−038 to 3.4e+038
+        System.out.println(yaBoat);
+
+        double decker = 14.39d;       //fractional numbers from 1.7e−308 to 1.7e+308
+        System.out.println(decker);
+
+
+        boolean onePieceGoated = true;
+        System.out.println(onePieceGoated);
+        boolean onePieceTooLong = false;
+        System.out.println(onePieceTooLong);
+
+
+        char TierListRating = 'S';
+        System.out.println(TierListRating);
+
+        int KSI = 72;
+        long ladda = KSI;
+        System.out.println(ladda);
+
+        double dutch = 23.27;
+        int entional = (int) dutch;
+
+        System.out.println(dutch);
+        System.out.println(entional);
+
+        int clap = 444/8;
+        System.out.println(clap);
+
+        String scripture = "Have you read One Piece?? \n Why not??\t What are you waiting for? \n Take the leap into a better life.";
+        System.out.println(scripture);
+
+
+        int egeral = (int)(Math.random() * 93);
+        System.out.println(egeral);
+
+    }
+
+}
